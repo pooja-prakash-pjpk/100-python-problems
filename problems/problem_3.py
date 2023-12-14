b662296434d946f14f8546cefc0f7cd6bd88527f
Pooja Prakash <pooja@Poojas-MacBook-Pro.local>
2023-12-14 13:14:09 -0800

    solution to problem 3

---
+++ problems/problem_3.py
@@ -0,0 +1,18 @@
+# Question3:
+#
+# With a given integral number n, write a program to generate
+# a dictionary that contains (i, i x i) such that is an integral number
+# between 1 and n (both included). and then the program should print the
+# dictionary.Suppose the following input is supplied to the program: 8
+# Then, the output should be:
+# {1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64}
+
+def square_dict(n):
+    my_dict = dict()
+    for n in range(1, n + 1):
+        my_dict.update({n: n * n})
+    print(my_dict)
+
+
+if __name__ == '__main__':
+    square_dict(8)
